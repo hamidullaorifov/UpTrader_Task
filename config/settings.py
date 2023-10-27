984b0d7c6f25d1d60bd29a853ec4cbe869d89bc5
name <email>
2023-10-27 14:48:59 +0500

    Changed for deployment

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t5@5i8zhfq6zxnc+9j-$3yh27b_8^01iqca2yxbg=f802!zqnt'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -73,13 +77,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = { 
+        'default': { 
+            'ENGINE': 'django.db.backends.postgresql', 
+            'NAME': os.environ.get('DB_NAME'), 
+            'USER': os.environ.get('DB_USER'), 
+            'PASSWORD': os.environ.get('DB_PASSWORD'), 
+            'HOST': os.environ.get('DB_HOST'), 
+            'PORT': os.environ.get('DB_PORT'), 
+        } 
+    }
 
 
 # Password validation
